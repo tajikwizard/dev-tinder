b9fc6eb4982d8f6be5012fbff64c6d472defd4c0
tajikwizard <abdulazizcodeshere@gmail.com>
2025-12-23 17:08:28 +0500

    fixed conflict

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,25 +2,57 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        trim:true,
+        minlength:5,
+        validate:{
+            validator:function(value){
+                return /^[A-Za-z]+$/.test(value); // Only letters
+            },
+            message: props => `${props.value} contains invalid characters. Only letters are allowed!`
+        }
     },
     lastName:{
-        type:String
+        type:String,
+        trim: true
     },
     age:{
-        type:Number
+        type:Number,
+        min:8,
+        max:80
     },
     gender:{
-        type:String
+        type:String,
+        enum: ['male', 'female', 'other']
     },
     email:{
-        type: String
+        type: String,
+        required:true,
+        unique:true,
+        lowercase: true,
+        trim: true
     },
     password:{
-        type:String
+        type:String,
+        required:true,
     },
+    photoUrl:{
+        type:String,
+        default: "https://example.com/default.png"   
+    },
+    desc:{
+        type:String,
+        default:"Hello! I'm new here."
+    },
+    skills:{
+        type:[String],
+        default:[]
+    }
 
-})
+},
+    { timestamps: true }
+)
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
